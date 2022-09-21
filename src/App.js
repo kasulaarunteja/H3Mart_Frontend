957ef810ec7332173a60695e9e44f585e583bc00
kasulaarunteja <kasulaarunteja@gmail.com>
2022-09-22 00:30:44 +0530

    done

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Coin from './components/Coin';
 
+
 function App() {
   return (
     <div className="App">
-     <h2>heello worled</h2>
+    <div className="img-logo">
+        <img className="logo" src="img/coincapLogo.svg" ></img>
+      </div>
      <Coin/>
     </div>
   );
--- src/components/Coin.jsx
+++ src/components/Coin.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getData } from "../Redux/action";
+import "../App.css"
 
 const Coin = () => {
   const [limit, setLimit] = useState(25);
@@ -13,6 +14,8 @@ const Coin = () => {
   useEffect(() => {
     dispatch(getData());
   }, [limit]);
+
+       
 
   return (
     <div>
@@ -38,60 +41,63 @@ const Coin = () => {
           </tr>
         </thead>
         <tbody>
+          
            {items.map((ele) => {
-            <tr>
-            <td>{ele.rank}</td>
-
-            <td>
-              <img
-                src={`https://assets.coincap.io/assets/icons/${ele.symbol.toLowerCase()}%402x.png`}
-                alt="crypto"
-                className="symbol"
-              />
-              {ele.name} <br />
-              <p style={{ marginLeft: "7%" }}>{ele.symbol}</p>
-            </td>
-
-            <td>${parseFloat(ele.priceUsd).toFixed(2)}</td>
-            <td>
-              {Math.abs(ele.marketCapUsd) >= 1.0e9
-                ? (Math.abs(ele.marketCapUsd) / 1.0e9).toFixed(2) + "b"
-                : Math.abs(ele.marketCapUsd) >= 1.0e6
-                ? (Math.abs(ele.marketCapUsd) / 1.0e6).toFixed(2) + "m"
-                : Math.abs(ele.marketCapUsd) >= 1.0e3
-                ? (Math.abs(ele.marketCapUsd) / 1.0e3).toFixed(2) + "k"
-                : Math.abs(ele.marketCapUsd)}
-            </td>
-
-            <td>{parseFloat(ele.vwap24Hr).toFixed(2)}</td>
-            <td>
-              {Math.abs(ele.supply) >= 1.0e9
-                ? (Math.abs(ele.supply) / 1.0e9).toFixed(2) + "b"
-                : Math.abs(ele.supply) >= 1.0e6
-                ? (Math.abs(ele.supply) / 1.0e6).toFixed(2) + "m"
-                : Math.abs(ele.supply) >= 1.0e3
-                ? (Math.abs(ele.supply) / 1.0e3).toFixed(2) + "k"
-                : Math.abs(ele.supply)}
-            </td>
-            <td>
-              {Math.abs(ele.volumeUsd24Hr) >= 1.0e9
-                ? (Math.abs(ele.volumeUsd24Hr) / 1.0e9).toFixed(2) + "b"
-                : Math.abs(ele.volumeUsd24Hr) >= 1.0e6
-                ? (Math.abs(ele.volumeUsd24Hr) / 1.0e6).toFixed(2) + "m"
-                : Math.abs(ele.volumeUsd24Hr) >= 1.0e3
-                ? (Math.abs(ele.volumeUsd24Hr) / 1.0e3).toFixed(2) + "k"
-                : Math.abs(ele.volumeUsd24Hr)}
-            </td>
-            {ele.changePercent24Hr < 0 ? (
-              <td className="coin-percent red">
-                {parseFloat(ele.changePercent24Hr).toFixed(2)}%
+            return (
+              <tr>
+              <td>{ele.rank}</td>
+              <td>
+                <img
+                  src={`https://assets.coincap.io/assets/icons/${ele.symbol.toLowerCase()}%402x.png`}
+                  alt="crypto"
+                  className="symbol"
+                  height="40px" w="40px"
+                />
+                {ele.name} <br />
+                <p style={{ marginLeft: "7%" }}>{ele.symbol}</p>
               </td>
-            ) : (
-              <td className="coin-percent green">
-                {parseFloat(ele.changePercent24Hr).toFixed(2)}%
+  
+              <td>${parseFloat(ele.priceUsd).toFixed(2)}</td>
+              <td>
+                {Math.abs(ele.marketCapUsd) >= 1.0e9
+                  ? (Math.abs(ele.marketCapUsd) / 1.0e9).toFixed(2) + "b"
+                  : Math.abs(ele.marketCapUsd) >= 1.0e6
+                  ? (Math.abs(ele.marketCapUsd) / 1.0e6).toFixed(2) + "m"
+                  : Math.abs(ele.marketCapUsd) >= 1.0e3
+                  ? (Math.abs(ele.marketCapUsd) / 1.0e3).toFixed(2) + "k"
+                  : Math.abs(ele.marketCapUsd)}
               </td>
-            )}
-          </tr>
+  
+              <td>{parseFloat(ele.vwap24Hr).toFixed(2)}</td>
+              <td>
+                {Math.abs(ele.supply) >= 1.0e9
+                  ? (Math.abs(ele.supply) / 1.0e9).toFixed(2) + "b"
+                  : Math.abs(ele.supply) >= 1.0e6
+                  ? (Math.abs(ele.supply) / 1.0e6).toFixed(2) + "m"
+                  : Math.abs(ele.supply) >= 1.0e3
+                  ? (Math.abs(ele.supply) / 1.0e3).toFixed(2) + "k"
+                  : Math.abs(ele.supply)}
+              </td>
+              <td>
+                {Math.abs(ele.volumeUsd24Hr) >= 1.0e9
+                  ? (Math.abs(ele.volumeUsd24Hr) / 1.0e9).toFixed(2) + "b"
+                  : Math.abs(ele.volumeUsd24Hr) >= 1.0e6
+                  ? (Math.abs(ele.volumeUsd24Hr) / 1.0e6).toFixed(2) + "m"
+                  : Math.abs(ele.volumeUsd24Hr) >= 1.0e3
+                  ? (Math.abs(ele.volumeUsd24Hr) / 1.0e3).toFixed(2) + "k"
+                  : Math.abs(ele.volumeUsd24Hr)}
+              </td>
+              {ele.changePercent24Hr < 0 ? (
+                <td className="coin-percent red">
+                  {parseFloat(ele.changePercent24Hr).toFixed(2)}%
+                </td>
+              ) : (
+                <td className="coin-percent green">
+                  {parseFloat(ele.changePercent24Hr).toFixed(2)}%
+                </td>
+              )}
+            </tr>
+            )
            })}
         </tbody>
       </table>
@@ -99,6 +105,7 @@ const Coin = () => {
         Load More
       </button>
     </div>
+    
   );
 };
 

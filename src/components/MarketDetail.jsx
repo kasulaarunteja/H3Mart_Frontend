7f6dd1b5ec07aca58a3734c83c455be679d1acc4
kasulaarunteja <kasulaarunteja@gmail.com>
2022-09-30 20:15:11 +0530

    h2

--- src/components/MarketDetail.jsx
+++ src/components/MarketDetail.jsx
@@ -11,19 +11,24 @@ const MarketDetail = () => {
         <h2>
           MARKET CAP <br /> $1.34T
         </h2>
+
         <h2>
           EXCHANGE VOL <br /> $55.04B
         </h2>
         <h2>
+
           ASSETS <br /> 2,295
         </h2>
+
         <h2>
           EXCHANGES <br /> 73
         </h2>
         <h2>
+
           MARKETS <br /> 13,989
         </h2>
         <h2>
+          
           BTC DOM INDEX <br /> 31.7%
         </h2>
       </div>
